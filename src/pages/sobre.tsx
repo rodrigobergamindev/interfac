4074c360d6c89848fd862b415bc6054c1b6c6f3d
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-08-30 15:20:04 -0300

    .

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,3 +1,6 @@
+import { Faq } from '../components/FAQ'
+import { Clientes } from '../components/Home/Clientes'
+import {Container, Header, Description, Contato, ButtonContato, Tab} from '../styles/styles.sistema'
 
 
 
@@ -6,7 +9,101 @@ export default function Sobre() {
 
 
     return (
-        <h1>Tá vindo</h1>
+        <Container>
+           
+            <Header>
+                <div>
+                <h1>INterfac</h1>
+                <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h3>
+                </div>
+            </Header>
+        
+
+        <Description>
+            <header>
+                <div></div>
+
+                <h1>APLICAÇÃO INterfac</h1>
+            </header>
+
+            
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+             when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+             typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
+             desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+             </p>
+          
+        </Description>
+
+        <Faq/>
+
+        <Clientes/>
+
+
+        <Tab>
+            <div className="containerTab">
+                <div className="title">
+                    <h2>0%</h2>
+                </div>
+
+                <ul>
+                    <li> <img src="/img/icons/check.png" alt=""/> Taxa de cancelamento </li>
+                    <li> <img src="/img/icons/check.png" alt=""/> Cobrança por atendimento telefônico </li>
+                    <li> <img src="/img/icons/check.png" alt=""/> Cobrança por atendimento por conexão remota</li>
+                </ul>
+
+
+                
+                </div>
+        </Tab>
+
+        <Tab>
+            <div className="containerTab">
+                <div className="title">
+                    <h2 className="limite">SEM</h2>
+                    <h2 className="limite">LIMITE</h2>
+                </div>
+
+                <ul>
+                    <li> <img src="/img/icons/check.png" alt=""/> Treinamento horário de início e término pré-agendado </li>
+                    <li> <img src="/img/icons/check.png" alt=""/> Suporte técnico via telefone </li>
+                    <li> <img src="/img/icons/check.png" alt=""/> Suporte técnico via acesso remoto</li>
+                </ul>
+
+
+                
+                </div>
+        </Tab>
+
+
+
+        <Contato>
+            <div className="containerContato">
+                <div className="title">
+                    <h2>Entre em contato</h2>
+                    <h4>Nossa equipe está pronta para atender as necessidades da sua empresa</h4>
+                
+
+                    <nav>
+                        <a href=""><img src="/img/contato/whatsapp.png" alt="whatsapp"/></a>
+                        <a href=""><img src="/img/contato/mail.png" alt="mail"/></a>
+                        <a href=""><img src="/img/contato/phone.png" alt="phone"/></a>
+                        <a href=""><img src="/img/contato/address.png" alt="address"/></a>
+                        
+                    </nav>
+                
+                </div>
+
+                <ButtonContato> <img src="/img/contato/contato.png" alt=""/> Solicite sua cotação</ButtonContato>
+
+
+                
+                </div>
+        </Contato>
+
+
+
+        </Container>
     )
 }
 
