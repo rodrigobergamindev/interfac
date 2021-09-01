9c839524c9f54d7d62023defbbf790c759bbc159
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-09-01 13:18:57 -0300

    .

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -12,21 +12,16 @@ export default function sendMail(req,res) {
     const {empresa} = req.body.campos
     const {segmento} = req.body.campos
 
-    console.log(req.body.campos)
-    console.log(process.env.MAIL_HOST)
-
     const transport = nodemailer.createTransport({
 
         
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
+        secure: true,
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS
         },
-        tls: {
-            rejectUnauthorized: false
-        }
     })
 
     transport.sendMail({
