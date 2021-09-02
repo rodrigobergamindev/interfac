789d71a641766ff8ff9fe3e8b232429dcd29eb4d
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-09-02 14:44:53 -0300

    .

--- src/components/Home/Clientes/Carousel/assets/carouselItems.ts
+++ src/components/Home/Clientes/Carousel/assets/carouselItems.ts
@@ -1,23 +1,58 @@
 export const carouselItems = [
     {   
         url: '/',
-        icon: '/img/clientes/sodimac.png'
+        icon: '/img/clientes/agaisom.png'
     },
     {   
         url: '/',
-        icon: '/img/clientes/carrefour.png'
+        icon: '/img/clientes/aneli.jpg'
     },
     {   
         url: '/',
-        icon: '/img/clientes/dell.png'
+        icon: '/img/clientes/carburely.png'
     },
     {   
         url: '/',
-        icon: '/img/clientes/mercadocar.png'
+        icon: '/img/clientes/jangada.jpg'
     },
     {   
         url: '/',
-        icon: '/img/clientes/walmart.png'
+        icon: '/img/clientes/miati.jpg'
     },
-
+    {   
+        url: '/',
+        icon: '/img/clientes/mineirao.png'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/monegato.png'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/mreis.png'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/multivarejo.jpg'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/muralha.jpg'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/perez.jpeg'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/rodrigotto.jpg'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/sw.jpg'
+    },
+    {   
+        url: '/',
+        icon: '/img/clientes/tnn.jpeg'
+    },
 ]
