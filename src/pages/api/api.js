88cf477cd4e00e012cf1ad758b77589c450f7d0e
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2021-09-02 14:04:23 -0300

    .

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -37,18 +37,6 @@ export default function sendMail(req,res) {
         
         `
     }).then(response => {
-        if(response.status === 200) {
-            transport.sendMail({
-                from: process.env.MAIL_FROM,
-                to: email,
-                replyTo: process.env.MAIL_FROM,
-                subject: `INterfac - Obrigado pelo Interesse! ✔️`,
-                html: `
-                <h3>Olá ${nome}, agradecemos o seu interesse e em breve entraremos em contato!</h3>
-                
-                `
-            })
-        }
         res.send(response)
     }).catch(error => [
         res.send(error)
